69f6b99e7e3d88d35c41181896bff7342d56b05e
AnaRamos2110 <ana.beatriz055@hotmail.com>
2025-02-27 20:29:27 -0300

    Subindo o primeiro projeto

---
+++ 27-02-25.py
@@ -0,0 +1,23 @@
+print("Olá mundo")
+
+modelo = 'Ford'
+print('O modelo do meu carro', modelo)
+
+print('\n')
+print("Eu amo estudar no Senac")
+print('')
+
+texto = "Por lei dirigir é permitido para maiores de:"
+idade = 18
+print(texto, idade)
+
+print("Estou aprendndo Python!")
+
+nome = "Godoy"
+cidade = 'Sbo'
+print("Meu nome é", nome, 'e eu moro em', cidade)
+
+a = 5
+b = 10
+soma = a + b
+print("A soma de", a,"e", b, "é", soma)
