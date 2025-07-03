61e76795b3cd57370524a359f57e88c15e0d6ec1
ANA BEATRIZ RAMOS <ana.bramos4@senacsp.edu.br>
2025-07-02 21:36:49 -0300

    Primeira automação em Python :)

---
+++ desk-auto.py
@@ -0,0 +1,13 @@
+#Automação -- existem bibliotecas de terceiros para ajudar neste processo !!
+import pyautogui
+import time
+import subprocess  
+
+#1- PASSO: ABRIR O BLOCO DE NOTAS (É UMA APLICAÇÃO DESKTOP!)
+
+subprocess.Popen('notepad.exe')
+time.sleep(2) #Para automatizar algo é sempre importante um tempo de espera para não ter delay!
+
+#2- PASSO:  DIGITAR UM TEXTO    
+pyautogui.write("Oii, Este eh um teste automatizado!", interval=0.1)
+
